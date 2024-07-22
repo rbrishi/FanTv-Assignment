1ad805885b28d0f6c2ca35eeff8d9d38e2537f9e
Rishabh Bajpai <98521911+rbrishi@users.noreply.github.com>
2024-07-22 15:51:04 +0530

    Add files via upload

---
+++ src/components/FrameComponent1.jsx
@@ -0,0 +1,156 @@
+import PropTypes from "prop-types";
+import "./FrameComponent1.css";
+
+const FrameComponent1 = ({ className = "" }) => {
+  return (
+    <div className={`frame-wrapper1 ${className}`}>
+      <div className="fancloud-cdn-network-revolutio-parent">
+        <h1 className="fancloud-cdn-network">
+          FanCloud CDN network revolutionizes video infrastructure with
+          community-driven Innovation
+        </h1>
+        <div className="frame-wrapper2">
+          <div className="frame-parent4">
+            <div className="fancloud-pioneers-a-new-era-of-wrapper">
+              <div className="fancloud-pioneers-a1">
+                FanCloud pioneers a new era of connectivity with its
+                decentralised content delivery network (dCDN), leveraging a
+                global network of community-powered nodes. By harnessing a
+                decentralised architecture, dCDN drastically reduces data
+                transfer and compute costs while enhancing load time. Join
+                FanCloud’s dCDN network and experience the future of seamless,
+                secure and community- driven consumption
+              </div>
+            </div>
+            <div className="ellipse-group">
+              <div className="frame-child1" />
+              <div className="frame-child2" />
+              <img className="globe-icon" alt="" src="/globe@2x.png" />
+              <div className="wrapper-ellipse-2714">
+                <img
+                  className="wrapper-ellipse-2714-child"
+                  loading="lazy"
+                  alt=""
+                  src="/ellipse-2714@2x.png"
+                />
+              </div>
+              <div className="wrapper-ellipse-2715">
+                <img
+                  className="wrapper-ellipse-2715-child"
+                  loading="lazy"
+                  alt=""
+                  src="/ellipse-2715@2x.png"
+                />
+              </div>
+              <div className="wrapper-ellipse-2717">
+                <img
+                  className="wrapper-ellipse-2717-child"
+                  loading="lazy"
+                  alt=""
+                  src="/ellipse-2717@2x.png"
+                />
+              </div>
+              <div className="wrapper-ellipse-2721">
+                <img
+                  className="wrapper-ellipse-2721-child"
+                  alt=""
+                  src="/ellipse-2721@2x.png"
+                />
+              </div>
+              <div className="wrapper-ellipse-2722">
+                <img
+                  className="wrapper-ellipse-2722-child"
+                  alt=""
+                  src="/ellipse-2722@2x.png"
+                />
+              </div>
+              <div className="wrapper-ellipse-2719">
+                <img
+                  className="wrapper-ellipse-2719-child"
+                  loading="lazy"
+                  alt=""
+                  src="/ellipse-2719@2x.png"
+                />
+              </div>
+              <div className="wrapper-ellipse-2724">
+                <img
+                  className="wrapper-ellipse-2724-child"
+                  loading="lazy"
+                  alt=""
+                  src="/ellipse-2724@2x.png"
+                />
+              </div>
+              <img
+                className="page-1-icon"
+                loading="lazy"
+                alt=""
+                src="/page-1@2x.png"
+              />
+              <div className="subtract-parent">
+                <img className="subtract-icon1" alt="" src="/subtract-1.svg" />
+                <div className="wrapper-ellipse-2712">
+                  <img
+                    className="wrapper-ellipse-2712-child"
+                    alt=""
+                    src="/ellipse-2712@2x.png"
+                  />
+                </div>
+                <div className="wrapper-ellipse-2713">
+                  <img
+                    className="wrapper-ellipse-2713-child"
+                    loading="lazy"
+                    alt=""
+                    src="/ellipse-2713@2x.png"
+                  />
+                </div>
+                <div className="wrapper-ellipse-2716">
+                  <img
+                    className="wrapper-ellipse-2716-child"
+                    loading="lazy"
+                    alt=""
+                    src="/ellipse-2716@2x.png"
+                  />
+                </div>
+                <div className="wrapper-ellipse-2718">
+                  <img
+                    className="wrapper-ellipse-2718-child"
+                    loading="lazy"
+                    alt=""
+                    src="/ellipse-2718@2x.png"
+                  />
+                </div>
+                <img
+                  className="page-1-icon1"
+                  loading="lazy"
+                  alt=""
+                  src="/page-1-1@2x.png"
+                />
+                <div className="ellipse-container">
+                  <div className="frame-child3" />
+                  <div className="frame-child4" />
+                </div>
+              </div>
+              <img className="frame-child5" alt="" src="/group-427319598.svg" />
+              <img className="frame-child6" alt="" src="/group-427319599.svg" />
+              <div className="ellipse-parent1">
+                <div className="frame-child7" />
+                <div className="frame-child8" />
+              </div>
+              <div className="ellipse-parent2">
+                <div className="frame-child9" />
+                <div className="frame-child10" />
+              </div>
+              <img className="heart-icon" alt="" src="/heart.svg" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+FrameComponent1.propTypes = {
+  className: PropTypes.string,
+};
+
+export default FrameComponent1;
---
+++ src/components/FrameComponent2.jsx
@@ -0,0 +1,246 @@
+import PropTypes from "prop-types";
+import "./FrameComponent2.css";
+
+const FrameComponent2 = ({ className = "" }) => {
+  return (
+    <section className={`grid-lines-wrapper ${className}`}>
+      <div className="grid-lines">
+        <img
+          className="vertical-lines-icon"
+          alt=""
+          src="/vertical-lines@2x.png"
+        />
+        <div className="diagonal-lines">
+          <img className="diagonal-lines-child" alt="" src="/line-82.svg" />
+          <img className="diagonal-lines-item" alt="" src="/line-83.svg" />
+          <img className="diagonal-lines-inner" alt="" src="/line-84.svg" />
+          <img className="line-icon" alt="" src="/line-85.svg" />
+          <img className="diagonal-lines-child1" alt="" src="/line-86.svg" />
+          <img className="diagonal-lines-child2" alt="" src="/line-87.svg" />
+          <img className="diagonal-lines-child3" alt="" src="/line-88.svg" />
+          <img className="diagonal-lines-child4" alt="" src="/line-89.svg" />
+          <img className="diagonal-lines-child5" alt="" src="/line-90.svg" />
+          <img className="diagonal-lines-child6" alt="" src="/line-91.svg" />
+          <img className="diagonal-lines-child7" alt="" src="/line-92.svg" />
+          <img className="diagonal-lines-child8" alt="" src="/line-93.svg" />
+          <img className="diagonal-lines-child9" alt="" src="/line-94.svg" />
+          <img className="diagonal-lines-child10" alt="" src="/line-95.svg" />
+          <img className="diagonal-lines-child11" alt="" src="/line-96.svg" />
+          <img className="diagonal-lines-child12" alt="" src="/line-97.svg" />
+          <img className="diagonal-lines-child13" alt="" src="/line-98.svg" />
+          <img className="diagonal-lines-child14" alt="" src="/line-99.svg" />
+          <img className="diagonal-lines-child15" alt="" src="/line-100.svg" />
+          <img className="diagonal-lines-child16" alt="" src="/line-101.svg" />
+          <img className="diagonal-lines-child17" alt="" src="/line-102.svg" />
+          <img className="diagonal-lines-child18" alt="" src="/line-103.svg" />
+          <img className="diagonal-lines-child19" alt="" src="/line-104.svg" />
+          <img className="diagonal-lines-child20" alt="" src="/line-105.svg" />
+          <img
+            className="diagonal-lines-child21"
+            loading="lazy"
+            alt=""
+            src="/line-106.svg"
+          />
+          <img
+            className="diagonal-lines-child22"
+            loading="lazy"
+            alt=""
+            src="/line-107.svg"
+          />
+          <img
+            className="diagonal-lines-child23"
+            loading="lazy"
+            alt=""
+            src="/line-108.svg"
+          />
+          <img
+            className="diagonal-lines-child24"
+            loading="lazy"
+            alt=""
+            src="/line-109.svg"
+          />
+        </div>
+        <div className="diagonal-lines1">
+          <img className="diagonal-lines-child25" alt="" src="/line-82-1.svg" />
+          <img className="diagonal-lines-child26" alt="" src="/line-83-1.svg" />
+          <img className="diagonal-lines-child27" alt="" src="/line-84-1.svg" />
+          <img className="diagonal-lines-child28" alt="" src="/line-85-1.svg" />
+          <img className="diagonal-lines-child29" alt="" src="/line-86-1.svg" />
+          <img className="diagonal-lines-child30" alt="" src="/line-87-1.svg" />
+          <img className="diagonal-lines-child31" alt="" src="/line-88-1.svg" />
+          <img className="diagonal-lines-child32" alt="" src="/line-89-1.svg" />
+          <img className="diagonal-lines-child33" alt="" src="/line-90-1.svg" />
+          <img className="diagonal-lines-child34" alt="" src="/line-91-1.svg" />
+          <img className="diagonal-lines-child35" alt="" src="/line-92-1.svg" />
+          <img className="diagonal-lines-child36" alt="" src="/line-93-1.svg" />
+          <img className="diagonal-lines-child37" alt="" src="/line-94-1.svg" />
+          <img className="diagonal-lines-child38" alt="" src="/line-95-1.svg" />
+          <img className="diagonal-lines-child39" alt="" src="/line-96-1.svg" />
+          <img className="diagonal-lines-child40" alt="" src="/line-97-1.svg" />
+          <img className="diagonal-lines-child41" alt="" src="/line-98-1.svg" />
+          <img className="diagonal-lines-child42" alt="" src="/line-99-1.svg" />
+          <img
+            className="diagonal-lines-child43"
+            alt=""
+            src="/line-100-1.svg"
+          />
+          <img
+            className="diagonal-lines-child44"
+            alt=""
+            src="/line-101-1.svg"
+          />
+          <img
+            className="diagonal-lines-child45"
+            alt=""
+            src="/line-102-1.svg"
+          />
+          <img
+            className="diagonal-lines-child46"
+            alt=""
+            src="/line-103-1.svg"
+          />
+          <img
+            className="diagonal-lines-child47"
+            alt=""
+            src="/line-104-1.svg"
+          />
+          <img
+            className="diagonal-lines-child48"
+            alt=""
+            src="/line-105-1.svg"
+          />
+          <img
+            className="diagonal-lines-child49"
+            alt=""
+            src="/line-106-1.svg"
+          />
+          <img
+            className="diagonal-lines-child50"
+            alt=""
+            src="/line-107-1.svg"
+          />
+          <img
+            className="diagonal-lines-child51"
+            alt=""
+            src="/line-108-1.svg"
+          />
+          <img
+            className="diagonal-lines-child52"
+            alt=""
+            src="/line-109-1.svg"
+          />
+        </div>
+        <div className="grid-lines-inner">
+          <div className="frame-parent">
+            <div className="frame-group">
+              <div className="frame-container">
+                <div className="frame-div">
+                  <div className="no-of-node-operators-wrapper">
+                    <h1 className="no-of-node">No. of Node Operators</h1>
+                  </div>
+                  <img
+                    className="frame-item"
+                    loading="lazy"
+                    alt=""
+                    src="/group-427319596.svg"
+                  />
+                </div>
+                <div className="parent">
+                  <div className="div">2000</div>
+                  <div className="frame-inner" />
+                </div>
+              </div>
+              <div className="frame-parent1">
+                <div className="wrapper-subtract-parent">
+                  <div className="wrapper-subtract">
+                    <img className="subtract-icon" alt="" src="/subtract.svg" />
+                  </div>
+                  <div className="join-the-worlds-first-depin-parent">
+                    <div className="join-the-worlds">
+                      Join the world’s first DePIN: Revolutionizing Content
+                      Streaming with faster, Safer and Cheaper Access
+                    </div>
+                    <div className="frame-wrapper">
+                      <button className="rectangle-group">
+                        <div className="rectangle-div" />
+                        <div className="coming-soon-wrapper">
+                          <div className="coming-soon">Coming Soon</div>
+                        </div>
+                        <img
+                          className="vuesaxboldarrow-right-icon"
+                          alt=""
+                          src="/vuesaxboldarrowright.svg"
+                        />
+                      </button>
+                    </div>
+                    <div className="wrapper-group-427319595-parent">
+                      <div className="wrapper-group-427319595">
+                        <img
+                          className="wrapper-group-427319595-child"
+                          alt=""
+                          src="/group-427319595.svg"
+                        />
+                      </div>
+                      <div className="empowering">
+                        <h1 className="empowering-the-digital-container">
+                          <p className="empowering-the-digital">{`Empowering the Digital Era: `}</p>
+                          <p className="decentarlizing-entertainment">
+                            Decentarlizing Entertainment
+                          </p>
+                        </h1>
+                        <div className="wrapper-horizontal-line">
+                          <img
+                            className="horizontal-line-icon"
+                            alt=""
+                            src="/horizontal-line.svg"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                    <div className="wrapper-blob">
+                      <img className="blob-icon" alt="" src="/blob.svg" />
+                    </div>
+                  </div>
+                </div>
+                <div className="frame-parent2">
+                  <div className="frame-parent3">
+                    <div className="data-transferred-in-tb-last-3-wrapper">
+                      <div className="data-transferred-in-container">
+                        <span>{`Data Transferred in TB `}</span>
+                        <span className="last-30-days">(last 30 days)</span>
+                      </div>
+                    </div>
+                    <img
+                      className="group-icon"
+                      loading="lazy"
+                      alt=""
+                      src="/group-427319596.svg"
+                    />
+                  </div>
+                  <div className="ellipse-parent">
+                    <div className="ellipse-div" />
+                    <div className="div1">12,000</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="divider-wrapper">
+              <img
+                className="divider-icon"
+                loading="lazy"
+                alt=""
+                src="/vector-987.svg"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+FrameComponent2.propTypes = {
+  className: PropTypes.string,
+};
+
+export default FrameComponent2;
